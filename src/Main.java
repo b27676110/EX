fc07326553bc76df3ba9978b001d351c0fee7c61
b27676110 <105021036@live.asia.edu.tw>
2017-04-24 15:42:03 +0800

    105021036_20170424

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+public class Main{
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		Mainframe mf = new Mainframe();
+		mf.setVisible(true);
+
+	}
+
+}
---
+++ src/Mainframe.java
@@ -0,0 +1,38 @@
+
+import java.awt.*;
+import java.awt.event.*;
+
+import com.sun.glass.events.WindowEvent;
+
+public class Mainframe extends Frame {
+	private Button btnExit = new Button("Exit");
+
+	public Mainframe() {
+		init();
+	}
+
+	public void init() {
+		this.setSize(100, 100);
+		this.setLocation(100, 100);
+
+		  this.addWindowListener(new WindowAdapter() {
+			     public void windowClosing(WindowEvent ev) {
+				System.exit(0);
+
+			}
+		});
+		this.setBackground(new Color(128,255,128));
+		this.setLayout(null);
+		btnExit.setLocation(200,150);
+		btnExit.setSize(80,25);
+		
+		
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent a) {
+				System.exit(0);
+
+			}
+		});
+		this.add(btnExit);
+	}
+}
